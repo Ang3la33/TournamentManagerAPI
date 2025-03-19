c1a05edbc860713d66ff3b689a531d7d24074915
Angela Flynn <156027242+Ang3la33@users.noreply.github.com>
2025-03-19 19:15:48 -0230

    Created Member model using UUID and GenericGenerator to create a unique ID for each member, used lombok to generate getters, setters, a no arg constructor, and all args constructor - used @Column providing variables to ensure data integrity - established Many-toMany relationship with Tournament as multiple members can be in multiple tournaments and vice versa - defined key member attributes: name, address, email, phone, memberJoinDate, and membershipDurationInMonths

---
+++ src/main/java/com/tournamentmanager/model/Member.java
@@ -0,0 +1,46 @@
+package com.tournamentmanager.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.GenericGenerator;
+
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Member {
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
+    @Column(unique = true, nullable = false, updatable = false)
+    private String id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, length = 100)
+    private String address;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(unique = true, nullable = false)
+    private String phone;
+
+    @Column(nullable = false)
+    private LocalDate memberJoinDate;
+
+    @Column(nullable = false)
+    private int membershipDurationInMonths;
+
+    @ManyToMany(mappedBy = "members")
+    private Set<Tournament> tournaments = new HashSet<>();
+
+}
---
+++ src/main/java/com/tournamentmanager/model/Tournament.java
@@ -0,0 +1,4 @@
+package com.tournamentmanager.model;
+
+public class Tournament {
+}
